815ab54787ff8a6ffdf5ec22a5842e5714cf140b
변영화[주문&결제FE] <younghwa.byun@navercorp.com>
2025-04-28 18:47:26 +0900

    ✨ qr 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import {useEffect, useState} from 'react'
 import Barcode from './Barcode'
 import './App.css'
+import {QRCodeCanvas} from 'qrcode.react'
 
 const Device = () => {
   const [query, setQuery] = useState<string | null>(null)
   const [width, setWidth] = useState<string>('320')
+  const [mode, setMode] = useState<string>('qr') // 'qr' | 'barcode'
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search)
     setQuery(params.get('code'))
     setWidth(params.get('width') || '320')
+    setMode(params.get('mode') || 'qr')
   }, [])
 
-  return (
+  return mode === 'barcode' ? (
     <div>
       <h3>width: {width}</h3>
       <div
@@ -27,6 +30,21 @@ const Device = () => {
         {query && <Barcode code={query} height={70} />}
       </div>
       code: {query}
+    </div>
+  ) : (
+    <div>
+      <h3>width: {width}</h3>
+      <div
+        style={{
+          boxSizing: 'border-box',
+          padding: '20px',
+          border: '1px solid black',
+          display: 'inline-block',
+        }}
+      >
+        {query && <QRCodeCanvas value={query} size={+width} />}
+      </div>
+      <p>code: {query}</p>
     </div>
   )
 }
